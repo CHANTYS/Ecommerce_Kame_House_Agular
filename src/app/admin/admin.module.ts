408bcdac403b347529a84453e3364c65c6fb69fc
Jorge Romero <jor1196@gmail.com>
2024-08-23 10:18:48 -0300

    - add post-categories component - add admin service

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PostCategoryComponent } from './components/post-category/post-category.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DemoAngularMaterialModule } from '../DemoAngularMaterialModule';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostCategoryComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    DemoAngularMaterialModule
   ]
 })
 export class AdminModule { }
