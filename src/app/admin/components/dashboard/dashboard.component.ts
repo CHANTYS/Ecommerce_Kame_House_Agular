791a4931f726b23c7203c5351fab24e14f758685
Jorge Romero <jor1196@gmail.com>
2024-09-15 14:03:32 -0300

    Features - Admin - Dashboard - ProductDetail
    - add product details on admin dashboard

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../service/admin.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  products = new Array<any>();
+  
+  constructor(private adminService: AdminService) { }
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.products = new Array<any>();
+
+    this.adminService.getAllProducts().subscribe({
+      next: (res) => {
+        res.forEach((element: any) => {
+          element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
+          this.products.push(element);
+        });
+      }
+    });
+  }
 
 }
